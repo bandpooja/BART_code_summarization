61f2e8b8f8d89d8972580ebec7754e62006c4524
bandpooja <bandpooja@gmail.com>
2022-04-16 16:52:51 -0400

    check 9

--- slurm/one4all.sh
+++ slurm/one4all.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=4
 #SBATCH --gpus-per-node=1
 #SBATCH --time=24:00:00
-#SBATCH --output=/home/mjyothi/scratch/one4all/run1/log.out
+#SBATCH --output=/home/mjyothi/scratch/one4all/run1/%x-%j-log.out
 
 module load gcc/9.3.0 arrow cuda/11 python/3.8
 source /home/mjyothi/bart/bin/activate
