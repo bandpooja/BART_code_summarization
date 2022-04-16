0e4f246e0c254514706c2dac62eef66cab2a5b67
bandpooja <bandpooja@gmail.com>
2022-04-16 16:32:06 -0400

    check 9

--- slurm/one4all.sh
+++ slurm/one4all.sh
@@ -8,4 +8,5 @@
 
 module load gcc/9.3.0 arrow cuda/11 python/3.8
 source /home/mjyothi/bart/bin/activate
+export TRANSFORMERS_CACHE=/home/mjyothi/.cache
 python -m experiment.one4all.exp01 -o /home/mjyothi/scratch/heirarchical/run1 &> /home/mjyothi/scratch/one4all/run1/run.log
