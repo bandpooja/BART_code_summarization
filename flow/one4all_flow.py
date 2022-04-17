427bee8f9920c580bbe2ab834dd0c5a9cf8c3ed7
Pooja <59486401+bandpooja@users.noreply.github.com>
2022-04-17 19:01:13 -0400

    Update one4all_flow.py
    check

--- flow/one4all_flow.py
+++ flow/one4all_flow.py
@@ -16,7 +16,7 @@ class One4AllCodeSummarizationModel:
     def __init__(self, bart_model_name: str = "ncoop57/summarization-base-code-summarizer-java-v0",
                  languages: list = ['python', 'java', 'javascript', 'php'],
                  bart_model_dir: str = "", bart_tokenizer_dir: str = "", continue_training: bool = False,
-                 initial_weights_dir: str = None):
+                 initial_wts_dir: str = None):
         """
              Constructor
 
@@ -31,15 +31,15 @@ class One4AllCodeSummarizationModel:
         self.bart_model_dir = bart_model_dir
         self.languages = languages
         
-        if initial_weights_dir:
-            self.bart_model = BartForConditionalGeneration.from_pretrained(osp.join(initial_weights_dir, 'bart'))        
+        if initial_wts_dir:
+            self.bart_model = BartForConditionalGeneration.from_pretrained(osp.join(initial_wts_dir, 'bart'))        
         elif osp.exists(self.bart_model_dir) and continue_training:
             self.bart_model = BartForConditionalGeneration.from_pretrained(self.bart_model_dir)
         else:
             self.bart_model = BartForConditionalGeneration.from_pretrained(self.BART_MODEL_NAME)
 
         if initial_weights_dir:
-            self.tokenizer = AutoTokenizer.from_pretrained(osp.join(initial_weights_dir, 'bart_tokenizer'))        
+            self.tokenizer = AutoTokenizer.from_pretrained(osp.join(initial_wts_dir, 'bart_tokenizer'))        
         elif osp.exists(bart_tokenizer_dir) and continue_training:
             self.tokenizer = AutoTokenizer.from_pretrained(self.bart_tokenizer_dir)
         else:
