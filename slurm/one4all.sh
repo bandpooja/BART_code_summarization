a5babcdeb4fc5850f2fc34cd04fcb54f615f0657
bandpooja <bandpooja@gmail.com>
2022-04-19 01:44:22 -0400

    check 12

--- slurm/one4all.sh
+++ slurm/one4all.sh
@@ -8,4 +8,4 @@
 
 module load gcc/9.3.0 arrow cuda/11 python/3.8
 source /home/mjyothi/home/mjyothi/bart/bin/activate
-python -m experiment.one4all.exp01 -o /home/mjyothi/scratch/heirarchical/run1 --cache_dir /home/mjyothi/home/mjyothi/cache --initial_wts_dir /home/mjyothi/home/mjyothi/initial_wts &> /home/mjyothi/scratch/one4all/run1/run.log
+python -m experiment.one4all.exp01 -o /home/mjyothi/scratch/one4all/run1 --cache_dir /home/mjyothi/home/mjyothi/cache --initial_wts_dir /home/mjyothi/home/mjyothi/initial_wts &> /home/mjyothi/scratch/one4all/run1/run.log
