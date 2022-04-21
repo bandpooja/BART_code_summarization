384b19067aabd10a4c4179d37bd6873344d29dab
Pooja <59486401+bandpooja@users.noreply.github.com>
2022-04-21 09:37:47 -0400

    Update hierarchical_flow.py
    correction

--- flow/hierarchical_flow.py
+++ flow/hierarchical_flow.py
@@ -113,7 +113,7 @@ class HierarchicalCodeSummarizationModel:
         )
 
         trainer = pl.Trainer(distributed_backend=self.backend, num_sanity_val_steps=0,
-                             default_root_dir=self.bert_model_path, accelerator=self.backend
+                             default_root_dir=self.bert_model_path, accelerator=self.backend,
                              max_epochs=N_EPOCHS, gpus=gpus, progress_bar_refresh_rate=30,
                              callbacks=[EarlyStopping(monitor='val_loss', patience=3),
                                         ModelCheckpoint(
