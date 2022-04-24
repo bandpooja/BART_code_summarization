99fa1fa0edfd2d7526cd5d09235f0f390dbbb97b
Pooja <59486401+bandpooja@users.noreply.github.com>
2022-04-24 10:30:19 -0400

    Update bart_model.py
    check

--- models/summarization/bart_model.py
+++ models/summarization/bart_model.py
@@ -24,7 +24,7 @@ class SummarizerWithPretrainedTokenizer:
 
     def train(self, train_dataset, val_dataset, compute_metrics,
               n_epochs: int = 10, batch_sz: int = 16, label_smoothing: float = 0.1,
-              logging_steps: int = 500000):
+              logging_steps: int = 500):
 
         training_args = Seq2SeqTrainingArguments(
             output_dir=self.model_loc,
@@ -37,6 +37,7 @@ class SummarizerWithPretrainedTokenizer:
             label_smoothing_factor=label_smoothing,
             logging_dir=self.logs_loc,
             logging_steps=logging_steps,
+            saving_steps=500000,
             eval_accumulation_steps=100000
         )
 
@@ -87,7 +88,7 @@ class SummarizerWithCustomTokenizer:
 
     def train_model(self, train_dataset, val_dataset, compute_metrics,
                     n_epochs: int = 10, batch_sz: int = 16, label_smoothing: float = 0.1,
-                    logging_steps: int = 500000, gpus: int = 4):
+                    logging_steps: int = 500, gpus: int = 4):
         
         if gpus > 0:
             gpus = gpus
@@ -106,7 +107,8 @@ class SummarizerWithCustomTokenizer:
             label_smoothing_factor=label_smoothing,
             logging_dir=self.logs_loc,
             logging_steps=logging_steps,
-            eval_accumulation_steps=500000,
+            saving_steps=500000,
+            eval_accumulation_steps=100000,
         )
 
         self.trainer = Seq2SeqTrainer(
