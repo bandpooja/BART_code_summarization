940b3a73a938da2d80ca7b872ec418824e1fd763
Pooja <59486401+bandpooja@users.noreply.github.com>
2022-04-25 08:24:52 -0400

    Update exp01.py
    check

--- experiment/one4all/exp01.py
+++ experiment/one4all/exp01.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     # region set parameters for training
     # model_location = './One4All/'
     model_location = model_loc
-    BATCH_SZ = 128*4
+    BATCH_SZ = 64*4
     N_EPOCHS = 10
     gpus = 4
     # endregion
